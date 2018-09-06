df4acdf9ecdb11deb1c1d680bb27b205ae24ebb1
Petra Cvrljevic <pcvrljevic@Petras-MacBook-Air.local>
2018-09-06 14:56:57 +0200

    WebKit View replaced with Web View

--- Marvel/Controllers/CharacterDetailsViewController.swift
+++ Marvel/Controllers/CharacterDetailsViewController.swift
@@ -11,20 +11,20 @@ import WebKit
 import Kingfisher
 import MBProgressHUD
 
-class CharacterDetailsViewController: UIViewController, WKNavigationDelegate {
+class CharacterDetailsViewController: UIViewController, UIWebViewDelegate {
     
     @IBOutlet weak var characterImageView: UIImageView!
     @IBOutlet weak var characterName: UILabel!
     @IBOutlet weak var wikiButton: UIButton!
     
-    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var webView: UIWebView!
     
     var characterDetailViewModel : CharacterDetailViewModel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        webView.navigationDelegate = self
+
+        webView.delegate = self
         
         let character = characterDetailViewModel.characterViewModel
 
@@ -42,19 +42,15 @@ class CharacterDetailsViewController: UIViewController, WKNavigationDelegate {
         progressNavigation.isUserInteractionEnabled = false
         if let url = URL(string: characterDetailViewModel.characterViewModel.wikiURL) {
             print(url)
-            webView.load(URLRequest(url: url))
+            webView.loadRequest(URLRequest(url: url))
         }
     }
     
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+    func webViewDidFinishLoad(_ webView: UIWebView) {
         MBProgressHUD.hide(for: webView, animated: true)
     }
     
-    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
-        MBProgressHUD.hide(for: webView, animated: true)
-    }
-    
-    func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+    func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
         MBProgressHUD.hide(for: webView, animated: true)
         let alertVC = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
         alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
